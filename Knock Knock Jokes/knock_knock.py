0075cce03bd65d6b67b1deab0518201f262812e6
Lebogang Olifant <lebogangodeveloper@gmail.com>
2019-02-25 17:52:41 +0200

    display drawings

---
+++ Knock Knock Jokes/knock_knock.py
@@ -0,0 +1,10 @@
+def tell(intro, punchline):
+    print("knock knock")
+    print("who's there")
+    print(intro)
+    print(intro + "who")
+    print(punchline)
+
+    tell("Door", "Just open the door please!")
+    
+
--- drawings/my_drawings.py
+++ drawings/my_drawings.py
@@ -1,6 +1,11 @@
-from shapes import Triangle, Rectangle, Oval
+from shapes import Triangle, Rectangle, Oval, Paper
+
+rect1 = Rectangle()
 
 rect1.set_width(200)
 rect1.set_height(100)
 rect1.set_color("blue")
+
 rect1.draw()
+
+paper.display()
